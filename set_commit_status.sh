a60bd333e54b76d1eb8496356f0b8ccb9554bf58
Kamil Chudy <kchudy@teonite.com>
2018-03-08 14:26:38 +0100

    Updated status script

--- set_commit_status.sh
+++ set_commit_status.sh
@@ -5,5 +5,5 @@ DESCRIPTION=$2
 
 curl -X POST \
     https://api.github.com/repos/kchudy/gitlab-ci-test/statuses/${CI_COMMIT_SHA} \
-    -H "Authorization: token ${GITHUB_TOKEN}" \
+    -H 'Authorization: token '${GITHUB_TOKEN} \
     -d '{"state": "'${BUILD_STATE}'", "target_url": "http://localhost/root/gitlab-ci-test/pipelines/'${CI_PIPELINE_ID}'", "description": "'${DESCRIPTION}'", "context": "continuous-integration/gitlab"}'
